5f5ca54b30c7853aedf58dad5f7043f2d59cd64b
goddammit <katarzyna.kraus@erlang-solutions.com>
2020-05-13 12:47:41 +0200

    right before liveview

--- lib/pointing_poker_web/controllers/room_controller.ex
+++ lib/pointing_poker_web/controllers/room_controller.ex
@@ -5,7 +5,12 @@ defmodule PointingPokerWeb.RoomController do
 
   def new(conn, _params) do
     room_id = Base.encode16(:crypto.strong_rand_bytes(8))
-    
+
+    {:ok, _} =
+      DynamicSupervisor.start_child(PointingPoker.Room.Supervisor,
+    {PointingPoker.Room, %{room_id: room_id}}
+    )
+
     redirect(conn, to: "/room/#{room_id}" )
   end
 
